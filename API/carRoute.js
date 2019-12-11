d90936222c37c29e616f267515b8964d06917575
Krunal Patel <Pkrunal616@gmail.com>
2019-12-10 21:36:02 -0500

    MVP done

--- API/carRoute.js
+++ API/carRoute.js
@@ -17,5 +17,39 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    databass.select('*').from('carInfo').where({ id: req.params.id }).first()
+    .then(carID => {
+        res.status(200).json(carID)
+    })
+    .catch(err => {
+        console.log(err, 'from GET to get car by id')
+        res.status(500).json({
+            error: 'Can not get that car by that ID'
+        })
+    })
+})
+
+router.post('/', (req, res) => {
+    const body = req.body;
+
+    if(!body.vin || !body.make || !body.model || !body.mileage) {
+        res.status(400).json({
+            error: 'Please prove vin, make, model, or mileage to make a post'
+        })
+    }else{
+        databass.insert(body, 'id').into('carInfo')
+        .then(newData => {
+            res.status(201).json(newData)
+        })
+        .catch(err => {
+            console.log(err ,'from POST to add new car')
+            res.status(500).json({
+                error: 'can not add new car data in the databass'
+            })
+        })
+
+    }
+})
 
 module.exports = router;
