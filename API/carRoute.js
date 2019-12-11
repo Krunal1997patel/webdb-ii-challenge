0db4c37cd74f671c6a3f52baec4a9176e41ab3f0
Krunal Patel <Pkrunal616@gmail.com>
2019-12-10 21:18:37 -0500

    everything working great, need to add POST,PUT,DELETE

--- API/carRoute.js
+++ API/carRoute.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const databass = require('../data/dbConfig.js');
 
 router.use(express.json());
 
-
+router.get('/', (req, res) => {
+    databass.select('*').from('carInfo')
+    .then(cars =>{
+        res.status(200).json(cars)
+    })
+    .catch(err => {
+        console.log(err, 'from GET to get all cars')
+        res.status(500).json({
+            error: 'Can not get all the car data'
+        })
+    })
+})
 
 
 module.exports = router;
